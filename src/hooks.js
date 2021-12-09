931292075422f6a16512272ee826a13bbae49ca5
Rijosh <rijosh.ravi@phases.dk>
2021-12-09 19:33:19 +0530

    test hooks

--- src/hooks.js
+++ src/hooks.js
@@ -110,3 +110,138 @@ export const useUpdateEffect = (callback, dependencies) => {
     return callback();
   }, dependencies);
 };
+
+/**
+ * Set a readonly value
+ * @param {*} value
+ * @returns
+ */
+export const useReadonly = (value) => {
+  const valueRef = useRef(JSON.parse(JSON.stringify(value)));
+  return valueRef.current;
+};
+
+/**
+ * Get URL queries
+ * @returns - query data
+ */
+const getQuery = () => {
+  if (typeof window !== "undefined") {
+    return new URLSearchParams(window.location.search);
+  }
+  return new URLSearchParams();
+};
+
+/**
+ * Find the URL query value of the given key
+ * @param {*} key
+ * @returns
+ */
+const getQueryStringVal = (key) => {
+  return getQuery().get(key);
+};
+
+/**
+ * Hook to keep values in URL query
+ * @param {*} key
+ * @param {*} defaultVal
+ * @returns
+ */
+export const useQueryState = (key, defaultVal) => {
+  const defaultValue = getQueryStringVal(key) || defaultVal;
+  const [query, setQuery] = useState(defaultValue);
+
+  useEffect(() => {
+    setQuery(defaultValue);
+  }, [defaultValue]);
+
+  const updateUrl = (newVal) => {
+    const valueToStore = newVal instanceof Function ? newVal(query) : newVal;
+    setQuery(valueToStore);
+
+    const query = getQuery();
+
+    if (valueToStore.trim() !== "") {
+      query.set(key, valueToStore);
+    } else {
+      query.delete(key);
+    }
+
+    // This check is necessary if using the hook with Gatsby
+    if (typeof window !== "undefined") {
+      const { protocol, pathname, host } = window.location;
+      const newUrl = `${protocol}//${host}${pathname}?${query.toString()}`;
+      window.history.pushState({}, "", newUrl);
+    }
+  };
+
+  return [query, updateUrl];
+};
+
+// Get hash value from url
+const hash = window.location.hash.substr(1);
+
+/**
+ * Convert the hash values to object
+ * Split the hash with &
+ * Push the values as object
+ */
+const hashAsObject = hash
+  ? hash.split("&").reduce((res, item) => {
+      const parts = item.split("=");
+      res[parts[0]] = parts[1];
+      return res;
+    }, {})
+  : {};
+
+/**
+ * Check if the
+ * @param {*} key
+ * @returns
+ */
+const getHashValue = (key) => (key in hashAsObject ? hashAsObject[key] : "");
+
+/**
+ * Hook to keep the state in URL hash
+ * @param {*} key
+ * @param {*} defaultValue
+ * @returns
+ */
+export const useHashState = (key, defaultValue) => {
+  // keep state for handling hash
+  // set default value as the current hash value, if any, or the provided default value
+  const [hash, setHash] = useState(getHashValue(key) || defaultValue);
+
+  /**
+   * Update the hash value
+   * @param {*} newVal
+   */
+  const updateHash = (newVal) => {
+    // Get the new value
+    // if the value is an instance function, then get value from function
+    const valueToStore = newVal instanceof Function ? newVal(hash) : newVal;
+    // set the value to state
+    setHash(valueToStore);
+
+    // get the current hash values as object
+    let hashContent = hashAsObject;
+    // update the hash with new value
+    if (valueToStore.trim() !== "") {
+      hashContent[key] = valueToStore;
+    } else {
+      delete hashContent[key];
+    }
+
+    // generate array of updated hash values
+    let hashList = [];
+    for (const index in hashContent) {
+      if (key && key.trim() !== "") {
+        hashList.push(`${index}=${hashContent[index]}`);
+      }
+    }
+    // update the hash
+    window.location.hash = hashList.join("&");
+  };
+
+  return [hash, updateHash];
+};
